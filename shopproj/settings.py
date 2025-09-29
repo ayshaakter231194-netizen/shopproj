c972d2b944043961eacef5f7fc9ae7b793af125a
Aysha Akter <ayshaakter231194@gmail.com>
2025-09-29 15:58:46 +0600

    Update settings.py

--- shopproj/settings.py
+++ shopproj/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pad$3x^6s0^!31k6stv2c*@9+!1klzfik=d338xg_48e@o=006'
+SECRET_KEY = os.getenv("SECRET_KEY", "your-default-secret-key") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["store.onrender.com"]
+ALLOWED_HOSTS = [os.getenv("RENDER_EXTERNAL_HOSTNAME"), "localhost"]
 
 # Application definition
 
