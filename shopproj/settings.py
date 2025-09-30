555a603cda5e843d644bef630a20f26d4420e83a
Aysha Akter <ayshaakter231194@gmail.com>
2025-09-30 09:03:18 +0600

    Update settings.py

--- shopproj/settings.py
+++ shopproj/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 
 MEDIA_URL = '/media/'
